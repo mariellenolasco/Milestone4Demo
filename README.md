efaea7383c80672c2f4c3b623e6e44a98d580614
mariellenolasco <marielle.nolasco@revature.com>
2020-09-02 10:27:57 -0400

    added files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Milestone4Demo
-This is the base repo associates should have to complete Milestone 4
+# Milestone4Solution
+M4 working bashscript draft for checking pipelines
+<br>
+To get the build status of a pipeline in Azure follow the format: https://dev.azure.com/{organization}/{project}/_apis/build/status/{definition}?api-version=6.0-preview.1
+Documentation can be found here: https://docs.microsoft.com/en-us/rest/api/azure/devops/build/status/get?view=azure-devops-rest-6.0
+
+## Files
+### test.sh
+This is the draft bash script for testing if the pipeline builds. Includes logic that checks pipeline yml file to see if it actually has the necessary commands. For now, it's just build.
+
+### azure-pipelines.yml
+Test yml file to check if the the bash script works. Will need to be removed for actual batch, the bash script would need to change the pipeline file the bash script is testing. 
+
+### creds.txt
+This is where you put in your pipeline credentials. First line must be the organization, second line the project, and the third line will be the definition id of your pipeline.
---
+++ test.sh
@@ -0,0 +1,83 @@
+#!/bin/bash 
+organization=$(sed '1q;d' creds.txt)
+project=$(sed '2q;d' creds.txt)
+definition=$(sed '3q;d' creds.txt)
+http_response=$(curl -s -o response.txt -w "%{http_code}" https://dev.azure.com/${organization}/${project}/_apis/build/status/${definition}?api-version=6.0-preview.1)
+if [ $http_response = "200" ] 
+then
+    echo "Server responsive"
+    echo "Build Status:"
+    scount=$(grep -c 'succeeded' response.txt)
+    if [ $scount -gt 0 ] 
+    then      
+        missing=0
+        echo "Success!" 
+        echo "Checking yaml file"
+        echo "Checking for build command"
+        build=$(grep -c 'dotnet build' azure-pipelines.yml)
+        if [ $build -gt 0 ]
+        then 
+            #check if yml contains build command check if contains other commands
+            echo "Pipeline has build command"
+        else
+            echo "build command not found, pipeline flawed"
+            $missing++
+        fi
+        echo "Checking for testing"
+        test=$(grep -c 'dotnet test' azure-pipelines.yml)
+        if [ $test -gt 0 ]
+        then    
+            echo "Pipeline has testing"
+        else
+            echo "test command not found, pipeline flawed"
+            $missing++
+        fi
+        echo "Checking if sonar cloud is set up"
+        sonarc=$(grep -c 'SonarCloudPrepare@1' azure-pipelines.yml)
+        if [ $sonarc -gt 0 ]
+        then    
+            echo "Pipeline has sonar cloud set up"
+        else
+            echo "sonar cloud set up not found, pipeline flawed"
+            $missing++
+        fi
+        echo "Checking if code analysis was run"
+        codeanal=$(grep -c 'SonarCloudAnalyze@1' azure-pipelines.yml)
+        if [ $codeanal -gt 0 ]
+        then    
+            echo "Pipeline runs code analysis"
+        else
+            echo "code analysis run task not found, pipeline flawed"
+            $missing++
+        fi
+        echo "Checking if code analysis was published"
+        codeanalpub=$(grep -c 'SonarCloudPublish@1' azure-pipelines.yml)
+        if [ $codeanal -gt 0 ]
+        then    
+            echo "Pipeline publishes code analysis"
+        else
+            echo "code analysis publish task not found, pipeline flawed"
+            $missing++
+        fi 
+        echo "Checking if webapp was deployed"
+        deployed=$(grep -c 'AzureRmWebAppDeployment@4' azure-pipelines.yml)
+        if [ $deployed -gt 0 ]
+        then    
+            echo "Pipeline deploys webapp"
+        else
+            echo "webapp wasn't deployed"
+            $missing++
+        fi
+        if [ $missing -gt 0 ]
+        then
+            echo "More than one essential task missing from pipeline, Fail!"
+        else    
+            echo "Passed!"
+        fi
+    else
+        echo "Failed!"
+    fi
+
+else
+echo "Server unresponsive. Please check if your credentials are valid"
+fi
